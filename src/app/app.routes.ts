e02b4d9ce47159c5977239029fa450c02018b24e
Danilo <danilocarcosta@hotmail.com>
2026-09-11 02:00:50 -0300

    feat: implement lazy loading for Counter and Todo screens, add Home screen
    - Updated app.routes.ts to include lazy loading for Counter and Todo components.
    - Removed app.spec.ts as it is no longer needed.
    - Created shared components: Menu and Page for layout structure.
    - Added Home, Counter, and Todo screens with basic templates and styles.
    - Updated global styles to include Tailwind CSS and set background and text colors.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Home } from './screens/home/home';
+// import { Counter } from './screens/counter/counter';
+// import { Todo } from './screens/todo/todo';
+// não preciso importar pois estou fazendo 'lazy load' importando direto quando a rota é acessada
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'counter',
+        // component: Counter,
+        loadComponent() {
+            return import('./screens/counter/counter').then((m) => m.Counter);
+        }
+    },
+    {
+        path: 'todo',
+        // component: Todo,
+        loadComponent() {
+            return import('./screens/todo/todo').then((m) => m.Todo);
+        }
+    },
+];
