63ac670183ec5c816c42c20c78944ffb85f2ae46
Danilo <danilocarcosta@hotmail.com>
2026-09-11 21:18:48 -0300

    feat: add shared MenuItem and Title components, update screens to use them

--- src/app/screens/todo/todo.ts
+++ src/app/screens/todo/todo.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Title } from '../../components/shared/title/title';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [Title],
   templateUrl: './todo.html',
   styleUrl: './todo.css',
 })
